9d817dd56a4bc06ee0e8edf6324ad32ac427110c
Karl 'NaN' Wikström <karl@wikstrom.io>
2021-05-04 16:37:12 +0200

    Add more test cases for cursor movement in buffer

--- internal/govim/buffer_test.go
+++ internal/govim/buffer_test.go
@@ -33,6 +33,20 @@ func TestBuffer_MoveCursor(t *testing.T) {
 			},
 		},
 		{
+			name: "can move cursor up when there is another line",
+			fields: fields{
+				cursorX: 0,
+				cursorY: 1,
+				buffer:  []string{"ab", "cd"},
+			},
+			d: dirUp,
+			expected: fields{
+				cursorX: 0,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
+			},
+		},
+		{
 			name: "cannot move cursor below last line",
 			fields: fields{
 				cursorX: 0,
@@ -44,6 +58,20 @@ func TestBuffer_MoveCursor(t *testing.T) {
 				cursorX: 0,
 				cursorY: 0,
 				buffer:  []string{""},
+			},
+		},
+		{
+			name: "can move cursor down when there is another line",
+			fields: fields{
+				cursorX: 0,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
+			},
+			d: dirDown,
+			expected: fields{
+				cursorX: 0,
+				cursorY: 1,
+				buffer:  []string{"ab", "cd"},
 			},
 		},
 		{
@@ -61,6 +89,34 @@ func TestBuffer_MoveCursor(t *testing.T) {
 			},
 		},
 		{
+			name: "can move cursor left when there is another character",
+			fields: fields{
+				cursorX: 1,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
+			},
+			d: dirLeft,
+			expected: fields{
+				cursorX: 0,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
+			},
+		},
+		{
+			name: "moves cursor the second last character when when moving from position outside of line length",
+			fields: fields{
+				cursorX: 10,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
+			},
+			d: dirLeft,
+			expected: fields{
+				cursorX: 1,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
+			},
+		},
+		{
 			name: "cannot move cursor to the right of the last character on a line",
 			fields: fields{
 				cursorX: 0,
@@ -72,6 +128,20 @@ func TestBuffer_MoveCursor(t *testing.T) {
 				cursorX: 0,
 				cursorY: 0,
 				buffer:  []string{""},
+			},
+		},
+		{
+			name: "can move cursor right when there is another character",
+			fields: fields{
+				cursorX: 0,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
+			},
+			d: dirRight,
+			expected: fields{
+				cursorX: 1,
+				cursorY: 0,
+				buffer:  []string{"ab", "cd"},
 			},
 		},
 	}
