9d0550fcad20edbbfd3134acd24c787299a0c2a3
Karl 'NaN' Wikström <karl@wikstrom.io>
2021-05-05 18:13:24 +0200

    Fix tests to work after refactoring

--- internal/govim/frame_test.go
+++ internal/govim/frame_test.go
@@ -9,6 +9,8 @@ import (
 )
 
 func TestSimpleFrame_MoveCursor(t *testing.T) {
+	ss := tcell.NewSimulationScreen("UTF-8")
+	ss.SetSize(5, 5)
 	type fields struct {
 		screen tcell.Screen
 		buffer []string
@@ -19,141 +21,120 @@ func TestSimpleFrame_MoveCursor(t *testing.T) {
 		d dir
 	}
 	tests := []struct {
-		name     string
-		fields   fields
-		d        dir
-		expected fields
+		name                  string
+		fields                fields
+		d                     dir
+		expectedScreenCursorX int
+		expectedScreenCursorY int
 	}{
 		{
 			name: "cannot move SimpleCursor above first line",
 			fields: fields{
-				screen: tcell.NewSimulationScreen("UTF-8"),
+				screen: ss,
 				buffer: []string{""},
 				cursor: NewSimpleCursorAt(0, 0),
 				offset: 0,
 			},
-			d: dirUp,
-			expected: fields{
-				screen: tcell.NewSimulationScreen("UTF-8"),
-				buffer: []string{""},
-				cursor: NewSimpleCursorAt(0, 0),
-				offset: 0,
-			},
+			d:                     dirUp,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 0,
 		},
 		{
 			name: "can move SimpleCursor up when there is another line",
 			fields: fields{
-				screen: tcell.NewSimulationScreen("UTF-8"),
+				screen: ss,
 				buffer: []string{"ab", "cd"},
 				cursor: NewSimpleCursorAt(0, 1),
 				offset: 0,
 			},
-			d: dirUp,
-			expected: fields{
-				screen: tcell.NewSimulationScreen("UTF-8"),
+			d:                     dirUp,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 0,
+		},
+		{
+			name: "cannot move SimpleCursor below last line",
+			fields: fields{
+				screen: ss,
+				buffer: []string{""},
+				cursor: NewSimpleCursorAt(0, 0),
+				offset: 0,
+			},
+			d:                     dirDown,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 0,
+		},
+		{
+			name: "can move SimpleCursor down when there is another line",
+			fields: fields{
+				screen: ss,
 				buffer: []string{"ab", "cd"},
 				cursor: NewSimpleCursorAt(0, 0),
 				offset: 0,
 			},
+			d:                     dirDown,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 1,
 		},
-		//{
-		//	name: "cannot move SimpleCursor below last line",
-		//	fields: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{""},
-		//	},
-		//	d: dirDown,
-		//	expected: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{""},
-		//	},
-		//},
-		//{
-		//	name: "can move SimpleCursor down when there is another line",
-		//	fields: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//	d: dirDown,
-		//	expected: fields{
-		//		cursorX: 0,
-		//		cursorY: 1,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//},
-		//{
-		//	name: "cannot move SimpleCursor to the left of the first character on a line",
-		//	fields: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{""},
-		//	},
-		//	d: dirLeft,
-		//	expected: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{""},
-		//	},
-		//},
-		//{
-		//	name: "can move SimpleCursor left when there is another character",
-		//	fields: fields{
-		//		cursorX: 1,
-		//		cursorY: 0,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//	d: dirLeft,
-		//	expected: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//},
-		//{
-		//	name: "moves SimpleCursor the second last character when when moving from position outside of line length",
-		//	fields: fields{
-		//		cursorX: 10,
-		//		cursorY: 0,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//	d: dirLeft,
-		//	expected: fields{
-		//		cursorX: 1,
-		//		cursorY: 0,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//},
-		//{
-		//	name: "cannot move SimpleCursor to the right of the last character on a line",
-		//	fields: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{""},
-		//	},
-		//	d: dirRight,
-		//	expected: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{""},
-		//	},
-		//},
-		//{
-		//	name: "can move SimpleCursor right when there is another character",
-		//	fields: fields{
-		//		cursorX: 0,
-		//		cursorY: 0,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//	d: dirRight,
-		//	expected: fields{
-		//		cursorX: 1,
-		//		cursorY: 0,
-		//		buffer:  []string{"ab", "cd"},
-		//	},
-		//},
+		{
+			name: "cannot move SimpleCursor to the left of the first character on a line",
+			fields: fields{
+				screen: ss,
+				buffer: []string{""},
+				cursor: NewSimpleCursorAt(0, 0),
+				offset: 0,
+			},
+			d:                     dirLeft,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 0,
+		},
+		{
+			name: "can move SimpleCursor left when there is another character",
+			fields: fields{
+				screen: ss,
+				buffer: []string{"ab", "cd"},
+				cursor: NewSimpleCursorAt(1, 0),
+				offset: 0,
+			},
+			d:                     dirLeft,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 0,
+		},
+		{
+			name: "moves SimpleCursor the second last character when when moving from position outside of line length",
+			fields: fields{
+				screen: ss,
+				buffer: []string{"ab", "cd"},
+				cursor: NewSimpleCursorAt(10, 0),
+				offset: 0,
+			},
+			d:                     dirLeft,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 0,
+		},
+		{
+			name: "cannot move SimpleCursor to the right of the last character on a line",
+			fields: fields{
+				screen: ss,
+				buffer: []string{""},
+				cursor: NewSimpleCursorAt(0, 0),
+				offset: 0,
+			},
+			d:                     dirRight,
+			expectedScreenCursorX: 0,
+			expectedScreenCursorY: 0,
+		},
+		{
+			name: "can move SimpleCursor right when there is another character",
+			fields: fields{
+				screen: ss,
+				buffer: []string{"ab", "cd"},
+				cursor: NewSimpleCursorAt(0, 0),
+				offset: 0,
+			},
+			d:                     dirRight,
+			expectedScreenCursorX: 1,
+			expectedScreenCursorY: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -164,9 +145,12 @@ func TestSimpleFrame_MoveCursor(t *testing.T) {
 				offset: tt.fields.offset,
 			}
 			f.MoveCursor(tt.d)
-			assert.Equal(t, f.screen, tt.expected.screen)
-			assert.Equal(t, f.buffer, tt.expected.buffer)
-			assert.Equal(t, f.cursor, tt.expected.cursor)
+
+			ss := f.screen.(tcell.SimulationScreen)
+			cx, cy, vis := ss.GetCursor()
+			assert.True(t, vis)
+			assert.Equal(t, tt.expectedScreenCursorX, cx)
+			assert.Equal(t, tt.expectedScreenCursorY, cy)
 		})
 	}
 }
